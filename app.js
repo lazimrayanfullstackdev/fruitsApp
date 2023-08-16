b2d9a77eb7ac539a5481be9e6640054958962898
Lazim Rayan <lazimrayanfreelancer@gmail.com>
2023-08-16 16:52:32 +0600

    Update and Delete Operation Include

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//New Code
 const mongoose = require('mongoose');
 
 // Connecting to Database
@@ -6,15 +7,11 @@ mongoose.connect('mongodb://127.0.0.1:27017/FruitsDB', {
   useUnifiedTopology: true
 });
 
-// Event listener to execute code once the connection is open
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
 
   const fruitSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true,'why not insert a name?']
-    },
+    name: String,
     rating: {
       type: Number,
       min: 1,
@@ -25,49 +22,56 @@ mongoose.connection.once('open', () => {
 
   const Fruit = mongoose.model("Fruit", fruitSchema);
 
-  const peach = new Fruit({
-    name: "Peach",
-    rating: 7,
-    review: "Peach is good."
+  
+//   // const kiwi = new Fruit({
+//   //   name: "Kiwi",
+//   //   rating: 6,
+//   //   review: "Sour Taste"
+//   // });
+
+//   // const orange = new Fruit({
+//   //   name: "Orange",
+//   //   rating: 8,
+//   //   review: "I love oranges!"
+//   // });
+
+//   // const banana = new Fruit({
+//   //   name: "Banana",
+//   //   rating: 10,
+//   //   review: "Healthiest fruit ever."
+//   // });
+
+//   // // Inserting data using insertMany
+//   // // Fruit.insertMany([apple, kiwi, orange, banana]).then(function(fruits) {
+//   // //   console.log("Data inserted", fruits); // Success
+//   // //   mongoose.connection.close(); // Close the connection
+//   // // }).catch(function(error) {
+//   // //   console.log(error);
+//   // //   mongoose.connection.close(); // Close the connection on error as well
+//   // // });
+  const jackfruit = new Fruit({
+    rating: 8,
+    review: "Best energetic fruit!."
   });
 
-  // peach.save();
-
-
-  // const kiwi = new Fruit({
-  //   name: "Kiwi",
-  //   rating: 6,
-  //   review: "Sour Taste"
-  // });
-
-  // const orange = new Fruit({
-  //   name: "Orange",
-  //   rating: 8,
-  //   review: "I love oranges!"
-  // });
-
-  // const banana = new Fruit({
-  //   name: "Banana",
-  //   rating: 10,
-  //   review: "Healthiest fruit ever."
-  // });
-
-  // // Inserting data using insertMany
-  // // Fruit.insertMany([apple, kiwi, orange, banana]).then(function(fruits) {
-  // //   console.log("Data inserted", fruits); // Success
-  // //   mongoose.connection.close(); // Close the connection
-  // // }).catch(function(error) {
-  // //   console.log(error);
-  // //   mongoose.connection.close(); // Close the connection on error as well
-  // // });
-
-  // Finding and logging all fruits
   Fruit.find().then(function(fruits) {
     fruits.forEach(function(fruit) {
       console.log("Fruit name:", fruit.name);
-      mongoose.connection.close();
     });
+    mongoose.connection.close();
   }).catch(function(error) {
     console.log(error);
-  }); 
+  });
+
+//   Fruit.updateOne({ _id: "64dc983ff9bea84eef65ed22" }, { name: "Jackfruit" }).then(function(result) {
+//     console.log("Updated Name!");
+//   }).catch(function(error) {
+//     console.log(error);
+//   });
+
+    // Fruit.deleteOne({name:"Peach"}).then(function(fruit){
+    //   console.log("Deleted ");
+    // }).catch(function(error){
+    //   console.log(error);
+    // })
 });
